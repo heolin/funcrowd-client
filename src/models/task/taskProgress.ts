4b43646363a80af9db04df0beb9edea13c1c1713
Wojciech Wlodarczyk <heolin123@gmail.com>
2021-02-12 02:02:42 +0100

    Adding repositories. Working on taskRepository

---
+++ src/models/task/taskProgress.ts
@@ -0,0 +1,33 @@
+
+export interface ITaskProgress {
+    taskId: number,
+    itemsDone: number,
+    itemsCount: number,
+    progress: number,
+    status: string,
+    maxScore: number,
+    score: number
+}
+
+/**
+ * Stores information about user's progress for a given task
+ */
+export default class TaskProgress implements ITaskProgress {
+    constructor(public taskId: number, public itemsDone: number, public itemsCount: number,
+                public progress: number, public status: string, public maxScore: number,
+                public score: number) {}
+                
+
+    static fromJson(data: ITaskProgress) {
+        let object = new TaskProgress(
+            data.taskId,
+            data.itemsDone,
+            data.itemsCount,
+            data.progress,
+            data.status,
+            data.maxScore,
+            data.score
+        );
+        return object;
+    }
+}
---
+++ src/repositories/taskRepository.ts
@@ -0,0 +1,43 @@
+import Task, { ITask } from "../models/task/task";
+import TaskProgress, { ITaskProgress } from "../models/task/taskProgress";
+import SessionManager from "../session/sessionManager";
+
+
+export default class TaskRepository {
+    
+    constructor(private _sessionManager: SessionManager) {}
+
+    async list(missionId: number): Promise<Task[]> {
+        const response = await this._sessionManager.get(
+            "missions/" + missionId + "/tasks/");
+
+        let tasks: Task[] = response.data.map(
+            (data: Object) => Task.fromJson(data as ITask));
+        return tasks;
+    }
+
+    async get(taskId: number): Promise<Task> {
+        const response = await this._sessionManager.get(
+            "tasks/" + taskId + "/");
+
+        let task = Task.fromJson(response.data);
+        return task;
+    }
+
+    async progressList(missionId: number): Promise<TaskProgress[]> {
+        const response = await this._sessionManager.get(
+            "missions/" + missionId + "/tasks/progress");
+
+        let tasks: TaskProgress[] = response.data.map(
+            (data: Object) => TaskProgress.fromJson(data as ITaskProgress));
+        return tasks;
+    }
+
+    async progress(taskId: number): Promise<TaskProgress> {
+        const response = await this._sessionManager.get(
+            "tasks/" + taskId + "/progress/");
+
+        let task = TaskProgress.fromJson(response.data);
+        return task;
+    }
+}
